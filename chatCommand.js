f6d06aedde764fef1e3a7f266b1a238a98c7a46f
Stevecoding <91769571+ssakspirit@users.noreply.github.com>
2024-09-25 20:41:16 +0900

    chatCommand.js

--- chatCommand.js
+++ chatCommand.js
@@ -1,22 +1,23 @@
-import * as server from "@minecraft/server" 
-import * as ui from "@minecraft/server-ui" 
+import * as server from "@minecraft/server"
+import * as ui from "@minecraft/server-ui"
 
-const { world } = server; 
+const { world } = server;
 // 커스텀 명령어 
-world.afterEvents.chatSend.subscribe(e => { 
+world.beforeEvents.chatSend.subscribe(e => {
 
-    const msg = e.message 
+    const msg = e.message
 
-    if (msg == "명령어1") { 
-        e.sender.runCommand("say 명령어1을 실행함") 
-    } 
-    if (msg == "명령어2") { 
-        e.sender.runCommand("say 명령어2를 실행함") 
-        e.sender.runCommand("give @s apple") 
-    } 
-    if (msg == "명령어3") { 
-        e.sender.runCommand("say 명령어3을 실행함") 
-        e.sender.runCommand("effect @s speed") 
-        e.sender.runCommand("") 
-    } 
+    if (msg == "명령어1") {
+        e.cancel = true; //입력한 명령어가 안보이게함
+        e.sender.runCommandAsync("say 명령어1을 실행함")
+    }
+    if (msg == "명령어2") {
+        e.sender.runCommandAsync("say 명령어2를 실행함")
+        e.sender.runCommandAsync("give @s apple")
+    }
+    if (msg == "명령어3") {
+        e.sender.runCommandAsync("say 명령어3을 실행함")
+        e.sender.runCommandAsync("effect @s speed")
+        e.sender.runCommandAsync("")
+    }
 })
