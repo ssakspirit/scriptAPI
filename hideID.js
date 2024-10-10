367d43bdc705561662dfe6b3da29b23f9013ddc3
Stevecoding <91769571+ssakspirit@users.noreply.github.com>
2024-10-11 07:36:14 +0900

    Update hideID.js

--- hideID.js
+++ hideID.js
@@ -1,7 +1,11 @@
+// Minecraft 서버 모듈에서 world와 system을 가져옵니다.
 import { world, system } from "@minecraft/server"
 
+// 2틱마다 실행되는 간격 함수를 설정합니다.
 system.runInterval(() => {
+    // 월드의 모든 플레이어에 대해 반복합니다.
     for (const player of world.getAllPlayers()) {
+        // 각 플레이어의 이름 태그를 빈 문자열로 설정합니다.
         player.nameTag = ""
     }
 }, 2)
