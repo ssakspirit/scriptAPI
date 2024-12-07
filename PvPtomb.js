e6cf8ab523ef70079c77fbba8c2568d884a133bd
Stevecoding <91769571+ssakspirit@users.noreply.github.com>
2024-12-07 09:53:12 +0900

    Update PvPtomb.js

--- PvPtomb.js
+++ PvPtomb.js
@@ -1,5 +1,23 @@
 import { world, system } from "@minecraft/server";
 
+/**
+ * PvP Tombstone System
+ * 
+ * 사용법:
+ * 1. PvP 서버에 이 애드온을 적용합니다.
+ * 2. 플레이어가 사망하면 자동으로 묘비가 생성되고 관전자 모드로 변경됩니다.
+ * 3. 다른 플레이어가 묘비를 파괴하면 사망한 플레이어가 부활합니다.
+ * 
+ * 관리자 명령어:
+ * - !부활 <플레이어명>: 지정한 플레이어를 강제로 부활시킵니다.
+ * - 예시: !부활 Steve
+ * 
+ * 주의사항:
+ * - 관리자 명령어는 OP 권한이 있는 플레이어만 사용할 수 있습니다.
+ * - 부활 시 플레이어는 묘비가 있던 위치로 텔레포트됩니다.
+ * - 사망한 플레이어는 관전자 모드가 되어 다른 플레이어와 상호작용할 수 없습니다.
+ */
+
 // 아머스탠드와 플레이어 매핑을 저장할 Map
 const tombMap = new Map();
 // 파티클 효과 인터벌을 저장할 Map
@@ -34,7 +52,8 @@ world.beforeEvents.chatSend.subscribe((event) => {
         const dimension = world.getDimension("overworld");
         
         // 묘비 제거 및 플레이어 부활
-        dimension.runCommandAsync(`kill @e[type=armor_stand,name="${graveName}"]`).then(() => {
+        dimension.runCommandAsync(`execute as @e[type=armor_stand,name="${graveName}"] at @s run tp ${targetName} ~ ~ ~`).then(() => {
+            dimension.runCommandAsync(`kill @e[type=armor_stand,name="${graveName}"]`);
             targetPlayer.runCommandAsync("gamemode survival @s");
             targetPlayer.nameTag = targetPlayer.name;
             targetPlayer.sendMessage("§a관리자에 의해 부활되었습니다!");
@@ -65,7 +84,7 @@ world.afterEvents.entityDie.subscribe((event) => {
         entity.runCommandAsync("gamemode spectator @s");
         
         // 사망한 플레이어의 이름 위에 표시
-        entity.nameTag = "§c☠ §f" + entity.name + " §c☠";
+        entity.nameTag = "��c☠ §f" + entity.name + " §c☠";
         
         // 모든 플레이어에게 사망 메시지 표시
         world.getAllPlayers().forEach(player => {
@@ -94,6 +113,10 @@ world.afterEvents.entityDie.subscribe((event) => {
             
             // 아머스탠드가 없다면 (부숴졌다면)
             if (armorStands.length === 0 && tombMap.has(entity.name)) {
+                // 마지막 묘비 위치로 텔레포트
+                const lastLocation = tombMap.get(entity.name).location;
+                entity.teleport(lastLocation);
+                
                 // 플레이어 권한 변경
                 entity.runCommandAsync("gamemode survival @s");
                 // 이름태그 원래대로
@@ -117,7 +140,11 @@ world.afterEvents.entityDie.subscribe((event) => {
             }
             // 아머스탠드가 있다면 매핑에 추가
             else if (armorStands.length > 0 && !tombMap.has(entity.name)) {
-                tombMap.set(entity.name, armorStandCheck);
+                // 위치 정보를 포함하여 저장
+                tombMap.set(entity.name, {
+                    interval: armorStandCheck,
+                    location: armorStands[0].location
+                });
             }
         }, 10);
         
