dc1d6a8292b64bdad18e91309bd848528590c1e0
Stevecoding <91769571+ssakspirit@users.noreply.github.com>
2025-07-04 09:13:29 +0900

    Update guildManager.js

--- guildManager.js
+++ guildManager.js
@@ -262,7 +262,7 @@ function createGuildUI(player) {
         .title("길드 생성")
         .textField("길드 이름을 입력하세요:", "길드 이름")
         .textField("길드 설명을 입력하세요:", "길드 설명")
-        .toggle("뒤로 가기", false);
+        .toggle("뒤로 가기", { defaultValue: false });
 
     form.show(player).then((response) => {
         if (response.canceled) return;
@@ -302,7 +302,7 @@ function joinGuildUI(player) {
     const form = new ModalFormData()
         .title("길드 가입 요청")
         .dropdown("가입을 요청할 길드를 선택하세요:", guildList)
-        .toggle("뒤로 가기", false);
+        .toggle("뒤로 가기", { defaultValue: false });
 
     form.show(player).then((response) => {
         if (response.canceled) return;
@@ -957,7 +957,7 @@ function openGuildDeletionUI(player) {
         const form = new ModalFormData()
             .title("길드 삭제")
             .dropdown("삭제할 길드 선택", guildNames)
-            .toggle("뒤로 가기", false);
+            .toggle("뒤로 가기", { defaultValue: false });
 
         form.show(player).then((response) => {
             if (response.canceled) {
